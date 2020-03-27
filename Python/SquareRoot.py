ed5c47a1b25c54e6ab199ecd03c1d4eae920ae06
ShortEmperor <ShortEmperor194@gmail.com>
2020-03-27 12:49:33 -0600

    Update SquareRoot.py

--- Python/SquareRoot.py
+++ Python/SquareRoot.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python
 
+import os
 import Algorithms as alg
+def main():
+    os.system("clear")
+    try:
+        print(f'Este programa retornara la raiz cudrada de algun numero \n Se puede elegir entre diferentes algoritmos para llevar a cabo la tarea')
 
-print(f'Welcome to the program')
+        print('Algoritmos: \n1.-Busqueda Binaria\n2.-Enumeracion exhaustiva\n3.-Aproximacion de soluciones')
 
+        algoritmo = int(input('Que algoritmo quiere usar? \n>>'))
+
+        objetivo = int(input("Escriba el numero del que desea obtener la raiz cuadrada: \n>>"))
+
+        if algoritmo == 2:
+            alg.Enum(objetivo)
+
+        elif algoritmo == 3:
+            alg.Aprox(objetivo)
+
+        else:
+            alg.Binary(objetivo)
+
+        repeat = input('Quiere repetir la operacion (Y/n)?')
+        if repeat.upper() == 'Y':
+            main()
+        else:
+            os.system("clear")
+            exit()
+
+    except ValueError:
+        print(f'Por favor selecciona un algoritmo!')
+
+repeat = 'N'
+if __name__=='__main__':
+    main()
