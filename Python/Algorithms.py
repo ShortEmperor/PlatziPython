321a0cc7fc93048cabad9e76392ea7dfdc02ce2c
Fabricio Escalante <ShortEmperor194@gmail.com>
2020-03-28 12:28:45 -0600

    Comentarios sobre el funcionamiento de las funciones

--- Python/Algorithms.py
+++ Python/Algorithms.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 def Aprox(objetivo):
+    """
+    Ejecuta un algoritmo de aproximacion de soluciones
+    Es un poco lento y no muy recomendado
+    objetivo    Integeer    Numero del cual queremos obtener la raiz
+
+    """
     epsilon = 0.001
     paso = epsilon**2
     respuesta = 0.0
@@ -14,6 +20,12 @@ def Aprox(objetivo):
         print(f'La raiz cuadrada de {objetivo} es {respuesta}')
 
 def Binary(objetivo):
+    """
+    Utiliza el algoritmo de busqueda binaria
+    El mas efectivo de todos
+    objetivo    Integeer    Numero del cual queremos obtener la raiz
+
+    """
     epsilon = 0.0001
     bajo = 0.0
     alto = max(0.0, objetivo)
@@ -31,6 +43,12 @@ def Binary(objetivo):
     print(f'La raiz cuadrada de {objetivo} es {respuesta}')
 
 def Enum(objetivo):
+    """
+    Hace una enumeracion exahustiva de posibles soluciones hasta encontrar la correcta
+    No es muy precisa
+    objetivo    Integeer    Numero del cual queremos obtener la raiz
+
+    """
     answer = 0
 
     while answer**2 < objetivo:
